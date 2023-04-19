e8d8125d74f2950376d5d22eb9cd0b7efa8143f2
Alex Khomenok <exleonardo@mail.ru>
2023-04-19 20:15:47 +0300

    Format grid

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="app-wrapper">
+			<header className="header">
+				<img src="https://assets.turbologo.ru/blog/ru/2018/03/18170838/prozrachniy-logo-800x575.png"></img>
+			</header>
+			<nav className="nav">
+				<div>
+					<a>Profiles</a>
+				</div>
+				<div>
+					<a>Messages</a>
+				</div>
+				<div>
+					<a>News</a>
+				</div>
+				<div>
+					<a>Music</a>
+				</div>
+				<div>
+					<a>Settings</a>
+				</div>
+			</nav>
+			<div className="content">Main content</div>
+		</div>
+	);
 }
 
 export default App;
