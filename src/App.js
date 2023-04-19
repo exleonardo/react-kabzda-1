cc5c4144730911f4ab8e539c1491939ebf5b1827
Alex Khomenok <exleonardo@mail.ru>
2023-04-19 20:39:42 +0300

    Add Componet

--- src/App.js
+++ src/App.js
@@ -1,29 +1,44 @@
 import "./App.css";
+import Header from "./Components/Header";
 
 function App() {
 	return (
 		<div className="app-wrapper">
-			<header className="header">
-				<img src="https://assets.turbologo.ru/blog/ru/2018/03/18170838/prozrachniy-logo-800x575.png"></img>
-			</header>
+			<Header />
 			<nav className="nav">
 				<div>
-					<a>Profiles</a>
+					<a href="#">Profiles</a>
 				</div>
 				<div>
-					<a>Messages</a>
+					<a href="#">Messages</a>
 				</div>
 				<div>
-					<a>News</a>
+					<a href="#">News</a>
 				</div>
 				<div>
-					<a>Music</a>
+					<a href="#">Music</a>
 				</div>
 				<div>
-					<a>Settings</a>
+					<a href="#">Settings</a>
 				</div>
 			</nav>
-			<div className="content">Main content</div>
+			<div className="content">
+				<div>
+					<img
+						src="https://cdn.tripster.ru/thumbs2/f8b8dd24-c646-11ed-b9bb-b6ff1f9232bb.1220x600.jpeg"
+						alt="content"
+					></img>
+				</div>
+				<div>ava + descr</div>
+				<div>
+					My Post
+					<div>new Post</div>
+					<div>
+						<div>post 1</div>
+						<div>post 2</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
