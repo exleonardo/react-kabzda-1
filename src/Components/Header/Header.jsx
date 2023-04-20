6fa2ea872c77036070f1cdcc4df64da779b9312b
Alex Khomenok <exleonardo@mail.ru>
2023-04-20 20:26:34 +0300

    Разбивка на стили

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import "./Header.css";
+
+function Header() {
+	return (
+		<header className="header">
+			<img
+				alt="logo"
+				src="https://assets.turbologo.ru/blog/ru/2018/03/18170838/prozrachniy-logo-800x575.png"
+			></img>
+		</header>
+	);
+}
+export default Header;
