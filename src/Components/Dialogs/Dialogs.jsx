3c2dd31b2f565daff5395e4956ed5b49b9e286c9
Alex Khomenok <exleonardo@mail.ru>
2023-04-22 20:41:25 +0300

    fix верстки

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -1,6 +1,21 @@
 import s from "./Dialog.module.css";
 
 const Dialogs = (props) => {
-	return <div className={s.content}>Dialogs</div>;
+	return (
+		<div className="dialogs">
+			<div className="dialogs-item">
+				<div className="dialog">Alex</div>
+				<div className="dialog">Artem</div>
+				<div className="dialog">Sveta</div>
+				<div className="dialog">Victor</div>
+				<div className="dialog">Valeryi</div>
+			</div>
+			<div className="messages">
+				<div className="className">Hi</div>
+				<div className="className">How is your It-camasutra</div>
+				<div className="className">Yo</div>
+			</div>
+		</div>
+	);
 };
 export default Dialogs;
