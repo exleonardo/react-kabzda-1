e240e7bba342d96685546e4072fea8c0a64a98f9
Alex Khomenok <exleonardo@mail.ru>
2023-04-22 22:18:48 +0300

    Дробление компоненты profile

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -1,15 +1,11 @@
 import MyPosts from "./MyPosts";
 import s from "./Profile.module.css";
+import ProfileInfo from "./ProfileInfo/ProfileInfo";
+
 const Profile = () => {
 	return (
 		<div>
-			<div>
-				<img
-					src="https://cdn.tripster.ru/thumbs2/f8b8dd24-c646-11ed-b9bb-b6ff1f9232bb.1220x600.jpeg"
-					alt="content"
-				></img>
-			</div>
-			<div>ava + descr</div>
+			<ProfileInfo />
 			<MyPosts />
 		</div>
 	);
---
+++ src/Components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -0,0 +1,17 @@
+// import s from "./ProfileInfo.module.css";
+
+const ProfileInfo = () => {
+	return (
+		<div>
+			<div>
+				<img
+					src="https://cdn.tripster.ru/thumbs2/f8b8dd24-c646-11ed-b9bb-b6ff1f9232bb.1220x600.jpeg"
+					alt="content"
+				></img>
+			</div>
+			<div>ava + descr</div>
+		</div>
+	);
+};
+
+export default ProfileInfo;
