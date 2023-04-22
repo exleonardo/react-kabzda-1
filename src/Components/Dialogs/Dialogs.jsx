f92de0031f3894ae10d60244c39bb3fca29a7063
Alex Khomenok <exleonardo@mail.ru>
2023-04-22 21:00:30 +0300

    Добавление диалогов

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -2,18 +2,18 @@ import s from "./Dialog.module.css";
 
 const Dialogs = (props) => {
 	return (
-		<div className="dialogs">
-			<div className="dialogs-item">
-				<div className="dialog">Alex</div>
-				<div className="dialog">Artem</div>
-				<div className="dialog">Sveta</div>
-				<div className="dialog">Victor</div>
-				<div className="dialog">Valeryi</div>
+		<div className={s.dialogs}>
+			<div className={s.dialogsItem}>
+				<div className={s.dialog + ` ` + s.active}>Alex</div>
+				<div className={s.dialog}>Artem</div>
+				<div className={s.dialog}>Sveta</div>
+				<div className={s.dialog}>Victor</div>
+				<div className={s.dialog}>Valeryi</div>
 			</div>
-			<div className="messages">
-				<div className="className">Hi</div>
-				<div className="className">How is your It-camasutra</div>
-				<div className="className">Yo</div>
+			<div className={s.messages}>
+				<div className={s.dialog}>Hi</div>
+				<div className={s.dialog}>How is your It-camasutra</div>
+				<div className={s.dialog}>Yo</div>
 			</div>
 		</div>
 	);
