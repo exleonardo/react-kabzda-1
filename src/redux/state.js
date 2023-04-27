c699a765aad5e21da20b97973d058b60f5711801
Alex Khomenok <exleonardo@mail.ru>
2023-04-27 20:51:46 +0300

    добавление экспорта функции

--- src/redux/state.js
+++ src/redux/state.js
@@ -21,4 +21,8 @@ let state = {
 	},
 };
 
+export let addPost = (postMessage) => {
+	let newPost = { id: 5, message: postMessage, likesCount: 0 };
+	state.profilePage.postData.push(newPost);
+};
 export default state;
