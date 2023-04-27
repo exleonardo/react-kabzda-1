0706e182003cc6bcd2736194bb37aee318b03a12
Alex Khomenok <exleonardo@mail.ru>
2023-04-27 20:35:04 +0300

    добавление переменной для хранения данных

--- src/Components/Profile/MyPosts/MyPosts.jsx
+++ src/Components/Profile/MyPosts/MyPosts.jsx
@@ -1,18 +1,22 @@
 import s from "./MyPosts.module.css";
 import Posts from "../MyPosts/Post/Posts";
+import React from "react";
 
 const MyPosts = (props) => {
 	debugger;
 	const postsElements = props.postsData.map((post) => (
 		<Posts message={post.message} likesCount={post.likesCount} />
 	));
-
+	let newPostElement = React.createRef();
+	let addPost = () => {
+		let text = newPostElement.current.value;
+	};
 	return (
 		<div>
 			<div className={s.postsBlock}>
 				<h3>My posts</h3>
-				<textarea></textarea>
-				<button>add Post</button>
+				<textarea ref={newPostElement}></textarea>
+				<button onClick={addPost}>add Post</button>
 			</div>
 			<div className={s.posts}>{postsElements}</div>
 		</div>
