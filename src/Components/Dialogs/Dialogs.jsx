c1e805009f10f7c68c42b508776de29d8bf0c882
Alex Khomenok <exleonardo@mail.ru>
2023-05-16 14:31:44 +0300

    изменения по проекту

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -1,6 +1,7 @@
 import s from "./Dialog.module.css";
 import Message from "./Message/Message";
 import DialogItem from "./DialogItem/DialogItem";
+import { sendMessageCreater, updateNewMessageBodyCreator } from "../../redux/state";
 
 const Dialogs = (props) => {
 	const messagesElement = props.state.messagesData.map((message) => <Message message={message.message} />);
@@ -8,10 +9,30 @@ const Dialogs = (props) => {
 		<DialogItem name={dialog.name} id={dialog.id} />
 	));
 
+	let newMessageBody = props.state.newMessageBody;
+	let onNewMessageChange = (e) => {
+		let body = e.target.value;
+		props.store.dispatch(updateNewMessageBodyCreator(body));
+	};
+	let onSendMessageClick = () => {
+		props.store.dispatch(sendMessageCreater());
+	};
 	return (
 		<div className={s.dialogs}>
 			<div className={s.dialogsItem}>{dialogsElement}</div>
 			<div className={s.messages}>{messagesElement}</div>
+			<div>
+				<div>
+					<textarea
+						value={newMessageBody}
+						onChange={onNewMessageChange}
+						placeholder="Enter your message"
+					></textarea>
+				</div>
+				<div>
+					<button onClick={onSendMessageClick}>Send</button>
+				</div>
+			</div>
 		</div>
 	);
 };
